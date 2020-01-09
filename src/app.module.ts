f85a6fad8a87adfea78f812206935e9c154e48bf
miyamae <yu1ro.mya@gmail.com>
2020-01-09 18:07:53 +0900

    fix: app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AdminsService } from './admins/admins.service';
 import { AdminsModule } from './admins/admins.module';
 import configuration from './config/configuration';
 
@@ -18,6 +17,6 @@ import configuration from './config/configuration';
     AdminsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, AdminsService],
+  providers: [AppService],
 })
 export class AppModule {}
